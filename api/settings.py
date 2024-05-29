3b29655f2b48c14c87de0d0e91d1181df2f93a25
Anders Therese <andersthereseholst@gmail.com>
2024-05-29 12:51:42 +0200

    settings

--- api/settings.py
+++ api/settings.py
@@ -40,7 +40,7 @@ REST_FRAMEWORK = {
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_COOKIE = 'jwt-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
