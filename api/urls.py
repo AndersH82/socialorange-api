145770f0761a5dfa8bcfa0aebe1f07e97c07fc14
Anders Therese <andersthereseholst@gmail.com>
2024-05-31 17:13:39 +0200

    fixed urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,14 +3,17 @@ from django.urls import path, include
 from .views import root_route, logout_route
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', root_route),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('comments/', include('comments.urls')),
-    path('followers/', include('followers.urls')),
-    path('likes/', include('likes.urls')),
-    path('posts/', include('posts.urls')),
-    path('profiles/', include('profiles.urls')),
+    path(
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+    ),
+    path('', include('profiles.urls')),
+    path('', include('posts.urls')),
+    path('', include('comments.urls')),
+    path('', include('likes.urls')),
+    path('', include('followers.urls')),
 ]
