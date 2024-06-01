5aa48dd9f5b8f3d47ed2383f0ac7ae3a79483963
Anders Therese <andersthereseholst@gmail.com>
2024-06-01 23:02:55 +0200

    settings

--- api/settings.py
+++ api/settings.py
@@ -77,7 +77,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
-    'localhost',
+    'localhost:3000',
     '8000-andersh82-socialorangea-wzh4t0giyfx.ws-eu114.gitpod.io',
     'socialorangeapi-e92b8d7040bd.herokuapp.com',
     '127.0.0.1',
@@ -133,7 +133,6 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
