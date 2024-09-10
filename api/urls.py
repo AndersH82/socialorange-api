c72cc4e7bc52f73fc4f2b4c1d38da1a34ec70462
Anders Holst <andersthereseholst@gmail.com>
2024-09-10 20:36:48 +0000

    dj rest auth

--- api/urls.py
+++ api/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/token/refresh/'),
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
