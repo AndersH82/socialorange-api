2e49ec3019df7ee2a7d45cbdd40de2d34947be3d
Anders Holst <andersthereseholst@gmail.com>
2024-09-12 08:31:05 +0000

    remake the settings.py

--- api/settings.py
+++ api/settings.py
@@ -1,17 +1,15 @@
-import os
-import re
 from pathlib import Path
+import os
 
 if os.path.exists('env.py'):
     import env
-
-APPEND_SLASH = True
 
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
@@ -19,15 +17,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    'DATETIME_FORMAT': '%d %b %Y',
+    )]
 }
-if 'DEV' not in os.environ:
-    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'rest_framework.renderers.JSONRenderer',
-    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
@@ -36,31 +27,25 @@ JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
 }
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '8000-andersh82-socialorangea-mh7bu9b273z.ws.codeinstitute-ide.net',
-    '3000-andersh82-socialorange-tsgvoq88woh.ws.codeinstitute-ide.net',
     'socialorangeapi-e92b8d7040bd.herokuapp.com',
-]
+    '8000-andersh82-socialorangea-mh7bu9b273z.ws.codeinstitute-ide.net'
+    ]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://3000-andersh82-socialorange-tsgvoq88woh.ws.codeinstitute-ide.net',
-    'https://8000-andersh82-socialorangea-mh7bu9b273z.ws.codeinstitute-ide.net',
-    'https://socialorange-8e2adf553f1c.herokuapp.com',
-    'https://socialorangeapi-e92b8d7040bd.herokuapp.com',
-    'https://localhost:3000',
-    'https://127.0.0.1',
-]
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -81,6 +66,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'corsheaders',
     'cloudinary',
+
     'profiles',
     'posts',
     'comments',
@@ -101,29 +87,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 ]
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://localhost:3000",
-    "https://socialorange-8e2adf553f1c.herokuapp.com",
-    "https://socialorangeapi-e92b8d7040bd.herokuapp.com",
-    "https://3000-andersh82-socialorange-tsgvoq88woh.ws.codeinstitute-ide.net",
-    "https://8000-andersh82-socialorangea-mh7bu9b273z.ws.codeinstitute-ide.net"
-]
-
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS.append(os.environ.get('CLIENT_ORIGIN'))
-
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    match = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE)
-    if match:
-        extracted_url = match.group(0)
-        CORS_ALLOWED_ORIGIN_REGEXES = [
-            rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-        ]
-
-CORS_ALLOW_CREDENTIALS = True
-
 ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
@@ -144,12 +107,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api.wsgi.application'
 
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -166,13 +137,28 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
