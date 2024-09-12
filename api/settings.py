1b6ab7bba2aed2f4a9e16d26a3d50b64ee04c7b1
Anders Holst <andersthereseholst@gmail.com>
2024-09-12 20:21:05 +0000

    add client origin

--- api/settings.py
+++ api/settings.py
@@ -60,6 +60,17 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS.append(os.environ.get('CLIENT_ORIGIN'))
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    match = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE)
+    if match:
+        extracted_url = match.group(0)
+        CORS_ALLOWED_ORIGIN_REGEXES = [
+            rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+        ]
+
 CSRF_TRUSTED_ORIGINS = [
     'https://3000-andersh82-socialorange-tsgvoq88woh.ws.codeinstitute-ide.net',
     'https://localhost:3000',
