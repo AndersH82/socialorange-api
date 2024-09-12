778656dba4c5f604554865cde40d9677ce301e98
Anders Holst <andersthereseholst@gmail.com>
2024-09-12 17:28:37 +0000

    change jwt settings

--- api/settings.py
+++ api/settings.py
@@ -28,8 +28,8 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-JWT_AUTH_SAMESITE = 'None'
-JWT_AUTH_HTTPONLY = False
+JWT_AUTH_SAMESITE = 'Strict'
+JWT_AUTH_HTTPONLY = True
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'api.serializers.CurrentUserSerializer'
