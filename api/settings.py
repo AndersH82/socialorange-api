03367fd8b7e0da52a698c8035c33242cbcaeec01
Anders Holst <andersthereseholst@gmail.com>
2024-09-12 12:31:05 +0000

    add pagination

--- api/settings.py
+++ api/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -17,8 +18,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
