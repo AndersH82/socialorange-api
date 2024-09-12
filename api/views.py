e5917224dac8f4ccd726bbfba228cd296cbf2daf
Anders Holst <andersthereseholst@gmail.com>
2024-09-12 18:08:51 +0000

    reset view and urls

--- api/views.py
+++ api/views.py
@@ -1,26 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.exceptions import ValidationError
-from rest_framework_simplejwt.tokens import RefreshToken
-from django.conf import settings
-from django.shortcuts import redirect
-from django.contrib.auth import logout
+from .settings import (
+    JWT_AUTH_COOKIE,
+    JWT_AUTH_REFRESH_COOKIE,
+    JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
+)
 
-@api_view(['POST'])
-def token_refresh_view(request):
-    refresh_token = request.data.get('refresh')
-
-    if not refresh_token:
-        raise ValidationError("Refresh token is required")
-
-    try:
-        RefreshToken.for_jwt_refresh_token(refresh_token)
-        # Delete the refresh token from the database
-        RefreshToken.blacklist(refresh_token)
-    except Exception as e:
-        return Response({'error': f"Failed to invalidate refresh token: {str(e)}"}, status=400)
-
-    return Response({"message": "Refresh token invalidated"})
 
 @api_view()
 def root_route(request):
@@ -28,13 +14,23 @@ def root_route(request):
         "message": "Welcome to my API for SocialOrange webpage!"
     })
 
-@api_view(['GET'])
-def clear_cookies_view(request):
-    response = HttpResponse(status=204)
-    response.delete_cookie(settings.JWT_AUTH_COOKIE, path='/')
-    response.delete_cookie(settings.JWT_AUTH_REFRESH_COOKIE, path='/')
-    return response
 
+@api_view(['POST'])
 def logout_route(request):
-    logout(request)
-    return redirect('/')
+    # Get the refresh token from the request
+    refresh_token = request.data.get('refresh')
+    
+    # Invalidate the refresh token
+    try:
+        RefreshToken.for_jwt_refresh_token(refresh_token)
+        # Delete the refresh token from the database
+        RefreshToken.blacklist(refresh_token)
+    except Exception as e:
+        return Response({'error': str(e)}, status=400)
+    
+    # Clear cookies
+    response = Response()
+    response.delete_cookie(JWT_AUTH_COOKIE, path='/')
+    response.delete_cookie(JWT_AUTH_REFRESH_COOKIE, path='/')
+    
+    return response
