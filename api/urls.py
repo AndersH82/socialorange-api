e7507bf976af3bb0b1c33aef3abb9b575375408d
Anders Holst <andersthereseholst@gmail.com>
2024-09-14 07:54:04 +0000

    change ack to dj-rest

--- api/urls.py
+++ api/urls.py
@@ -10,14 +10,13 @@ urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/logout/', logout_route),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-            static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
