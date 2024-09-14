61a5edf14fe1fbd18b201faa6488042d2a9ee623
Anders Holst <andersthereseholst@gmail.com>
2024-09-14 07:38:54 +0000

    static to urls path

--- api/urls.py
+++ api/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+            static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
