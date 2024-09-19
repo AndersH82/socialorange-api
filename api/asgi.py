7cb96de9e872e1fb2e08ea96d295ac6399613f51
Anders Holst <andersthereseholst@gmail.com>
2024-09-19 06:52:23 +0000

    views.py

--- api/asgi.py
+++ api/asgi.py
@@ -3,8 +3,8 @@ import sys
 from django.core.wsgi import get_asgi_application
 
 # Add the codex_api directory to the Python path
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'codex_api'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'api'))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codex_api.api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = get_asgi_application()
