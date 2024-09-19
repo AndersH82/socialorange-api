60c9d0062496eb74774c042ebfcd6bf1fc9038f2
Anders Holst <andersthereseholst@gmail.com>
2024-09-19 08:38:04 +0000

    httponly

--- api/settings.py
+++ api/settings.py
@@ -38,7 +38,7 @@ JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
-JWT_AUTH_HTTPONLY = False
+JWT_AUTH_HTTPONLY = True
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
